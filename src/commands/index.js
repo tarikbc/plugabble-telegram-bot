9369fa53738fae652905db378cc83f3ca8bd2b1e
rmunhoz <rogerio.j.munhoz@gmail.com>
2017-01-13 14:22:38 -0200

    Fixed some stuff

--- src/commands/index.js
+++ src/commands/index.js
@@ -1,21 +1,24 @@
 import enabled from './enabled';
-import { isAdmin } from '../lib/utils/admin';
+import {isAdmin} from '../lib/utils/admin';
 import {info} from '../lib/utils/log';
 let commands = new Map();
 
 const setUpBot = (bot, error) => {
-  enabled.forEach(command => commands.set(command, require(`./${command}`).default));
-  commands.forEach(command =>
-    bot.onText(command.regex, (msg, match) => {
-      if (!command.adminOnly || isAdmin(msg.from.id)) {
-        command.run(msg, match)
-        .then(reply =>
-          bot.sendMessage(msg.chat.id, reply.text, reply.options)
-          .catch(err => error('Erro ao enviar mensagem: ', err)))
-        .catch(err => bot.sendMessage(msg.chat.id, 'Erro ao executar comando: ', err));
-      } else { bot.sendMessage(msg.chat.id, 'Este comando só pode ser executado por admins.'); }
-    }));
-  info(`\nComandos habilitados: ${Array.from(commands.keys()).join(', ')}`);
+    enabled.forEach(command => commands.set(command, require(`./${command}`).default));
+    commands.forEach(command =>
+      bot.onText(command.regex, (msg, match) => {
+          if (!command.adminOnly || isAdmin(msg.from.id)) {
+              command.run(msg, match)
+                .then(reply => {
+                    bot.sendMessage(msg.chat.id, reply.text, reply.options)
+                      .catch(err => error(msg, `Erro ao enviar mensagem: ${err}`));
+                })
+                .catch(err => error(msg, err));
+          } else {
+              error(msg, 'Este comando só pode ser executado por admins.');
+          }
+      }));
+    info(`\nComandos habilitados: ${Array.from(commands.keys()).join(', ')}`);
 };
 
 export default {
--- src/lib/utils/admin.js
+++ src/lib/utils/admin.js
@@ -1,11 +1,10 @@
-const adminIds = []; // [userID]
+import adminIds from '../../admins';
 
-const notifyAdmins = (bot, txt) =>
-  adminIds.forEach(id =>
-    bot.sentMessage(id, txt, { parsed_mode: 'Markdown' }));
+const notifyAdmins = (bot, txt) => adminIds.forEach(id => bot.sentMessage(id, txt, {parsed_mode: 'Markdown'}));
 
 const isAdmin = userId => adminIds.indexOf(userId) >= 0;
 
 export {
   notifyAdmins,
-  isAdmin };
+  isAdmin
+};
